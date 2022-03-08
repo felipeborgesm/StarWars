6e16fc98629025ffec972863e8a5365fc07bb978
Yuri <ypdmarch@gmail.com>
2022-03-08 22:27:11 +0100

    feat: usecase ListRebels added

--- src/main/java/com/letscode/api/starwars/Application.java
+++ src/main/java/com/letscode/api/starwars/Application.java
@@ -4,6 +4,7 @@ import com.letscode.api.starwars.domains.Rebel;
 import com.letscode.api.starwars.gateways.persistence.RebelPersistenceGateWay;
 import com.letscode.api.starwars.gateways.persistence.impl.collection.RebelPersistenceCollectionGateWayImpl;
 import com.letscode.api.starwars.usecases.CreateRebel;
+import com.letscode.api.starwars.usecases.ListRebels;
 import com.letscode.api.starwars.usecases.ReportRebel;
 import com.letscode.api.starwars.usecases.UpdateRebel;
 import com.letscode.api.starwars.usecases.validators.CreateRebelValidator;
@@ -57,5 +58,22 @@ public class Application {
     //reportRebel.execute(rebel1);
 
     System.out.println(rebel1);
+
+    Rebel rebel2 =
+            Rebel.builder()
+                    .name("felipe2")
+                    .age(102)
+                    .gender("masculino")
+                    .inventory(List.of("comida2"))
+                    .location(List.of("2222", "2222", "Rio de Janeiro"))
+                    .build();
+
+    Rebel savedRebel2 = createRebel.execute(rebel2);
+    System.out.println(savedRebel2);
+
+    System.out.println("List all rebels:");
+    ListRebels listRebels = new ListRebels(rebelPersistenceGateWay);
+    List<Rebel> rebels = listRebels.execute();
+    System.out.println(rebels);
   }
 }
